78d7461b8a83537e1077f1cd9f28a0cb71c167f6
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-15 18:13:14 +0200

    Dodat comments.js file

---
+++ coments.js
@@ -0,0 +1,62 @@
+let coments = [
+      {
+    id: "1",
+    name: "John",
+    comment: "Great article!",
+    date: "2024-04-01"
+  },
+  {
+    id: "2",
+    name: "Emily",
+    comment: "I enjoyed reading this.",
+    date: "2024-04-01"
+  },
+  {
+    id: "3",
+    name: "David",
+    comment: "Very informative.",
+    date: "2024-03-31"
+  },
+  {
+    id: "4",
+    name: "Sophia",
+    comment: "Well written!",
+    date: "2024-03-31"
+  },
+  {
+    id: "5",
+    name: "Michael",
+    comment: "Thanks for sharing.",
+    date: "2024-03-30"
+  },
+  {
+    id: "6",
+    name: "Emma",
+    comment: "This helped me a lot.",
+    date: "2024-03-30"
+  },
+  {
+    id: "7",
+    name: "Daniel",
+    comment: "I have a question.",
+    date: "2024-03-29"
+  },
+  {
+    id: "8",
+    name: "Olivia",
+    comment: "Could you explain further?",
+    date: "2024-03-29"
+  },
+  {
+    id: "9",
+    name: "William",
+    comment: "Looking forward to more.",
+    date: "2024-03-28"
+  },
+  {
+    id: "10",
+    name: "Ava",
+    comment: "Nice work!",
+    date: "2024-03-28"
+  }
+]
