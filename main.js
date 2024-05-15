1516a097229c770fa93c5658bbcbd3616720c7d9
rlazar25 <119755392+rlazar25@users.noreply.github.com>
2024-05-15 21:55:16 +0200

    Napravljeno da se komentari iz objecta generisu na strani

---
+++ main.js
@@ -0,0 +1,17 @@
+let oneCommentDiv = document.querySelector('#one-comment');
+
+createComments();
+
+function createComments(){
+    let text = '';
+    coments.forEach(comment =>{
+        text += `<div class="col-10 offset-1 bg-light p-2 mb-2 shadow">
+            <span class="badge bg-primary" >${comment.name}</span>
+            <span class="badge bg-secondary" >${comment.date}</span>
+            <p class="p-3">${comment.comment}</p>
+        </div>
+    `.trim()
+})
+    oneCommentDiv.innerHTML = text
+}
+
